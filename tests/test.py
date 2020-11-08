adacb55e56437f88c31ba2a9d8ef0481b883f1de
Diogo Correia <me@diogotc.com>
2020-11-08 15:31:50 +0000

    Add some eh_tabuleiro tests
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- tests/test.py
+++ tests/test.py
@@ -5,14 +5,38 @@ import importlib
 target = importlib.import_module(sys.argv[1])
 
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
+class TestEhTabuleiro(unittest.TestCase):
+    def test_eh_tabuleiro1(self):
         """
-        Test that it can sum a list of integers
+        eh_tabuleiro(((1, 0, 0), (-1, 1, 0), (1, -1, -1))) = True
         """
-        data = [1, 2, 3]
-        result = target.sum(data)
-        self.assertEqual(result, 6)
+        data = ((1, 0, 0), (-1, 1, 0), (1, -1, -1))
+        result = target.eh_tabuleiro(data)
+        self.assertEqual(result, True)
+
+    def test_eh_tabuleiro2(self):
+        """
+        eh_tabuleiro(((1, 0, 0), ('0', 1, 0), (1, -1, -1))) = False
+        """
+        data = ((1, 0, 0), ('0', 1, 0), (1, -1, -1))
+        result = target.eh_tabuleiro(data)
+        self.assertEqual(result, False)
+
+    def test_eh_tabuleiro3(self):
+        """
+        eh_tabuleiro(((1, 0, 0), (-1, 1, 0), (1, -1))) = False
+        """
+        data = ((1, 0, 0), (-1, 1, 0), (1, -1))
+        result = target.eh_tabuleiro(data)
+        self.assertEqual(result, False)
+
+    def test_eh_tabuleiro4(self):
+        """
+        eh_tabuleiro(((True, 0, False), (-1, 1, True), (1, -1, False))) = False
+        """
+        data = ((True, 0, False), (-1, 1, True), (1, -1, False))
+        result = target.eh_tabuleiro(data)
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':
