57ea6ccadb2a3f67c82854fb09fa7517a16db96a
JoaoSilva2 <jdhsilva2000@gmail.com>
2020-11-09 14:00:09 +0000

    added 1 test for each rule

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
+
 import unittest
 import sys
 import importlib
+import os
 
 target = importlib.import_module(sys.argv[1])
 
@@ -39,22 +41,133 @@ class TestEhTabuleiro(unittest.TestCase):
         self.assertEqual(result, False)
 
 
+
+class TestEscolherPosicaoAuto(unittest.TestCase):
+    def test_escolher_posicao_auto1(self):
+        """
+        Testa a regra 1 (vitoria)
+        tableiro = ((1,0,0), (0,1,0), (0,0,0))
+        """
+        data = ((1,0,0), (0,1,0), (0,0,0))
+
+        result = target.escolher_posicao_auto(data, 1, 'perfeito')
+
+        self.assertEqual(result, 9)
+
+    def test_escolher_posicao_auto2(self):
+        """
+        Testa a regra 2 (bloqueio)
+        tableiro = ((1,0,0), (0,1,0), (0,0,0))
+        """
+        data = ((1,0,0), (0,1,0), (0,0,0))
+
+        result = target.escolher_posicao_auto(data, -1, 'perfeito')
+
+        self.assertEqual(result, 9)
+
+    def test_escolher_posicao_auto3(self):
+        """
+        Testa a regra 3 (bifurcacao)
+        tableiro = ((0,0,0), (1,-1,-1), (0,0,1))
+        """
+        data = ((0,0,0), (1,-1,-1), (0,0,1))
+
+        result = target.escolher_posicao_auto(data, 1, 'perfeito')
+
+        self.assertEqual(result, 7)
+
+    def test_escolher_posicao_auto4(self):
+        """
+        Testa a regra 4 (bloqueio de bifurcacao com numero bifurcacoes == 1)
+        tableiro = ((0,0,0), (1,-1,-1), (0,0,1))
+        """
+        data = ((0,0,0), (1,-1,-1), (0,0,1))
+
+        result = target.escolher_posicao_auto(data, -1, 'perfeito')
+
+        self.assertEqual(result, 7)
+ 
+    def test_escolher_posicao_auto5(self):
+        """
+        Testa a regra 4 (bloqueio de bifurcacao com numero bifurcacoes > 1)
+        tableiro = ((-1,0,0), (0,1,0), (0,0,1))
+        """
+        data = ((-1,0,0), (0,1,0), (0,0,1))
+
+        result = target.escolher_posicao_auto(data, -1, 'perfeito')
+
+        self.assertEqual(result, 3)
+
+    def test_escolher_posicao_auto6(self):
+        """
+        Testa a regra 5 (centro)
+        tableiro = ((0,0,0), (0,0,0), (0,0,0))
+        """
+        data = ((0,0,0), (0,0,0), (0,0,0))
+
+        result = target.escolher_posicao_auto(data, -1, 'perfeito')
+
+        self.assertEqual(result, 5)
+    
+    def test_escolher_posicao_auto7(self):
+        """
+        Testa a regra 6 (canto oposto)
+        tableiro = ((0,0,0), (0,-1,0), (0,0,1))
+        """
+        data = ((0,0,0), (0,-1,0), (0,0,1))
+
+        result = target.escolher_posicao_auto(data, -1, 'normal')
+
+        self.assertEqual(result, 1)
+
+    def test_escolher_posicao_auto8(self):
+        """
+        Testa a regra 7 (canto vazio)
+        tableiro = ((0,0,0), (0,-1,0), (0,0,1))
+        """
+        data = ((-1,0,0), (0,-1,0), (0,0,1))
+
+        result = target.escolher_posicao_auto(data, -1, 'basico')
+
+        self.assertEqual(result, 3)
+
+    def test_escolher_posicao_auto9(self):
+        """
+        Testa a regra 8 (lateral oposto)
+        tableiro = ((-1,0,-1), (0,-1,0), (1,0,1))
+        """
+        data = ((-1,0,-1), (0,-1,0), (1,0,1))
+
+        result = target.escolher_posicao_auto(data, -1, 'basico')
+
+        self.assertEqual(result, 2)
+
+
 class TestJogoDoGalo(unittest.TestCase):
     def test_jogo_do_galo1(self):
-        sys.stdin = open('testes_jogo_do_galo/test1_input.txt')
-        result = open("testes_jogo_do_galo/my_out.txt", "w+")
+        """
+        Testa jogo do galo com estrategia perfeita (e aplicada a regra 4)
+        """
+
+        dirname = os.path.dirname(__file__)
+        sys.stdin = open(os.path.join(
+            dirname, 'testes_jogo_do_galo/test1_input.txt'))
+        result = open(os.path.join(
+            dirname, 'testes_jogo_do_galo/my_out.txt'), "w+")
 
         sys.stdout = result
         target.jogo_do_galo('X', 'perfeito')
-        result.seek(0,0)
+        result.seek(0, 0)
 
-        answer = open('testes_jogo_do_galo/test1_answer.txt', 'r')
+        answer = open(os.path.join(
+            dirname, 'testes_jogo_do_galo/test1_answer.txt'), 'r')
 
         self.maxDiff = None
         self.assertEqual(answer.read(), result.read())
 
         result.close()
-        answer.close()    
+        answer.close()
+
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'])
